779b2c5caf132080f3f333b442e3cecc570a210f
syeda Faiza <syedafaiza2610@gmail.com>
2024-07-27 00:30:31 +0500

    adding videos

--- src/LandingPage/components/Highlights.js
+++ src/LandingPage/components/Highlights.js
@@ -1,19 +1,3 @@
-// import * as React from "react";
-// import Box from "@mui/material/Box";
-// import Card from "@mui/material/Card";
-// import Container from "@mui/material/Container";
-// import Grid from "@mui/material/Grid";
-// import Stack from "@mui/material/Stack";
-// import Typography from "@mui/material/Typography";
-// import AutoFixHighRoundedIcon from "@mui/icons-material/AutoFixHighRounded";
-// import ConstructionRoundedIcon from "@mui/icons-material/ConstructionRounded";
-// import QueryStatsRoundedIcon from "@mui/icons-material/QueryStatsRounded";
-// import SettingsSuggestRoundedIcon from "@mui/icons-material/SettingsSuggestRounded";
-// import SupportAgentRoundedIcon from "@mui/icons-material/SupportAgentRounded";
-// import ThumbUpAltRoundedIcon from "@mui/icons-material/ThumbUpAltRounded";
-// import Amna_image from "../../assets/images/amna1.png";
-
-
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
@@ -22,36 +6,41 @@ import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Amna_image from "../../assets/images/amna1.png";
+import Anas_image from "../../assets/images/anas .png";
+import Saad_image from "../../assets/images/saad.png";
+import Ghous_image from "../../assets/images/ghous ahmad.png";
 import "./Highlights.css";
 
 const items = [
   {
-    icon: Amna_image,
-    title: "Amna Shehzad",
-    description: "The Youngest Web Developer of Pakistan...",
+    icon: Anas_image,
+    title: "Anas Asif",
+    description: "The Youngest Web and Mobile App Developer of Pakistan...",
     videoUrl: "https://www.youtube.com/embed/-VIVJzVtiq8", // YouTube embed URL
   },
   {
     icon: Amna_image,
     title: "Amna Shehzad",
     description: "The Youngest Web Developer of Pakistan...",
-    videoUrl: "https://www.youtube.com/embed/-VIVJzVtiq8", // YouTube embed URL
+    videoUrl: "https://www.youtube.com/embed/rJotBlhaCC0",
+  },
+  {
+    icon: Saad_image,
+    title: "Saad Pasta",
+    description:
+      "Full-stack Software Engineer with 5+ years of experience in building high-quality,reliable full-stack Applications.",
+    videoUrl: "https://www.youtube.com/embed/6gDfrPOEkIU", // YouTube embed URL
+  },
+  {
+    icon: Ghous_image,
+    title: "Ghous Ahmad",
+    description:
+      "Full Stack Developer over 6 years of Experience in working on a diverse range of projects, from mobile apps to web apps",
+    videoUrl: "https://www.youtube.com/embed/CPo1aNag0kE",
   },
   {
     icon: Amna_image,
-    title: "Amna Shehzad",
-    description: "The Youngest Web Developer of Pakistan...",
-    videoUrl: "https://www.youtube.com/embed/-VIVJzVtiq8", // YouTube embed URL
-  },
-  {
-    icon: Amna_image,
-    title: "Amna Shehzad",
-    description: "The Youngest Web Developer of Pakistan...",
-    videoUrl: "https://www.youtube.com/embed/-VIVJzVtiq8", // YouTube embed URL
-  },
-  {
-    icon: Amna_image,
-    title: "Amna Shehzad",
+    title: "Jafar Aman",
     description: "The Youngest Web Developer of Pakistan...",
     videoUrl: "https://www.youtube.com/embed/-VIVJzVtiq8", // YouTube embed URL
   },
@@ -75,8 +64,9 @@ export default function Highlights() {
         pt: { xs: 4, sm: 12 },
         pb: { xs: 8, sm: 16 },
         color: "white",
-        // bgcolor: "#82bd3e",
-        backgroundImage: "linear-gradient(to right, #82bd3e, #4caf50)",
+        bgcolor: "white",
+        border: "1px solid black",
+        // backgroundImage: "linear-gradient(to right, #82bd3e, #4caf50)",
       }}
     >
       <Container
@@ -115,10 +105,10 @@ export default function Highlights() {
                   height: "300px", // Set a specific height for the card
                   border: "1px solid",
                   borderColor: "grey.800",
-                  background: "transparent",
-                  backgroundColor: "grey.900",
+                  // background: "transparent",
+                  backgroundColor: "white.900",
                   perspective: "1000px",
-                  // backgroundImage: "linear-gradient(to right, #82bd3e, #4caf50)",
+                  //backgroundImage: "linear-gradient(180 deg, #82bd3e, #4caf50)",
                 }}
               >
                 <Box
