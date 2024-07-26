163599ee3e5032bfdc23e32d3a7f2ac1d4ffe470
syeda Faiza <syedafaiza2610@gmail.com>
2024-07-27 03:38:40 +0500

    all work done in motivational blogs section

--- src/LandingPage/components/Highlights.js
+++ src/LandingPage/components/Highlights.js
@@ -5,30 +5,34 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import Amna_image from "../../assets/images/amna1.png";
 import Anas_image from "../../assets/images/anas .png";
 import Saad_image from "../../assets/images/saad.png";
 import Ghous_image from "../../assets/images/ghous ahmad.png";
+import Hunain_image from "../../assets/images/hunain.png";
+import Dilip_image from "../../assets/images/dilip.png";
 import "./Highlights.css";
 
 const items = [
   {
     icon: Anas_image,
     title: "Anas Asif",
-    description: "The Youngest Web and Mobile App Developer of Pakistan...",
+    description: "Our Shining Star...Youngest Web and Mobile App Developer",
     videoUrl: "https://www.youtube.com/embed/-VIVJzVtiq8", // YouTube embed URL
   },
   {
     icon: Amna_image,
     title: "Amna Shehzad",
-    description: "The Youngest Web Developer of Pakistan...",
+    description:
+      "Youngest Mern Stack Developer,Ui and Ux designer,Vector designer,Data Science Trainee,Power Bi Developer",
     videoUrl: "https://www.youtube.com/embed/rJotBlhaCC0",
   },
   {
-    icon: Saad_image,
-    title: "Saad Pasta",
+    icon: Hunain_image,
+    title: "Hunain Pasha",
     description:
-      "Full-stack Software Engineer with 5+ years of experience in building high-quality,reliable full-stack Applications.",
+      "Full-stack Software Engineer in building high-quality, reliable full-stack Applications.",
     videoUrl: "https://www.youtube.com/embed/6gDfrPOEkIU", // YouTube embed URL
   },
   {
@@ -39,19 +43,18 @@ const items = [
     videoUrl: "https://www.youtube.com/embed/CPo1aNag0kE",
   },
   {
-    icon: Amna_image,
-    title: "Jafar Aman",
-    description: "The Youngest Web Developer of Pakistan...",
-    videoUrl: "https://www.youtube.com/embed/-VIVJzVtiq8", // YouTube embed URL
+    icon: Saad_image,
+    title: "Saad Pasta",
+    description:
+      "Full-stack Software Engineer with 5+ years of experience.Developed 2 Open Source projects which have over 5000+ stars on GitHub",
+    videoUrl: "https://www.youtube.com/embed/jsap0rbLn5I",
   },
   {
-    icon: Amna_image,
-    title: "Amna Shehzad",
-    description: "The Youngest Web Developer of Pakistan...",
-    videoUrl: "https://www.youtube.com/embed/-VIVJzVtiq8", // YouTube embed URL
+    icon: Dilip_image,
+    title: "Dilip Armani",
+    description: "Full Stack Developer",
+    videoUrl: "https://www.youtube.com/embed/85tn03aEN4U",
   },
-
-  // Other items...
 ];
 
 export default function Highlights() {
@@ -64,9 +67,10 @@ export default function Highlights() {
         pt: { xs: 4, sm: 12 },
         pb: { xs: 8, sm: 16 },
         color: "white",
-        bgcolor: "white",
-        border: "1px solid black",
-        // backgroundImage: "linear-gradient(to right, #82bd3e, #4caf50)",
+        background: "linear-gradient(135deg, #97C39C, #75B07C)",
+
+        // background: "linear-gradient(to bottom, #97C39C, #75B07C)", // Updated background
+        boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)", // Added box shadow
       }}
     >
       <Container
@@ -84,10 +88,14 @@ export default function Highlights() {
             textAlign: { sm: "left", md: "center" },
           }}
         >
-          <Typography component="h1" variant="h4">
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ fontSize: "3rem", fontWeight: "bold", color: "#0958A7" }}
+          >
             Motivational Blogs
           </Typography>
-          <Typography variant="body1" sx={{ color: "black.400" }}>
+          <Typography variant="body1" sx={{ color: "#0958A7", fontSize: "1rem" }}>
             Explore why SMIT stands out!
           </Typography>
         </Box>
@@ -99,51 +107,62 @@ export default function Highlights() {
                 color="inherit"
                 component={Card}
                 spacing={1}
-                useFlexGap
                 sx={{
                   p: 3,
-                  height: "300px", // Set a specific height for the card
+                  height: "350px",
                   border: "1px solid",
-                  borderColor: "grey.800",
-                  // background: "transparent",
-                  backgroundColor: "white.900",
-                  perspective: "1000px",
-                  //backgroundImage: "linear-gradient(180 deg, #82bd3e, #4caf50)",
+                  borderColor: "#F0F7FE.400",
+                  backgroundColor: "#F8F8F8",
+                  borderRadius: "12px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
                 }}
               >
                 <Box
-                  className={`flip-card ${flippedIndex === index ? "flipped" : ""}`}
+                  sx={{ position: "relative", cursor: "pointer" }}
                   onClick={() => setFlippedIndex(flippedIndex === index ? null : index)}
-                  sx={{ cursor: "pointer", height: "100%", width: "100%" }}
                 >
-                  <Box className="flip-card-inner">
-                    <Box className="flip-card-front">
+                  {flippedIndex === index ? (
+                    <iframe
+                      width="100%"
+                      height="100%"
+                      src={`${item.videoUrl}?autoplay=1`}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      style={{ borderRadius: "12px" }}
+                    ></iframe>
+                  ) : (
+                    <>
                       <img
                         src={item.icon}
                         alt={item.title}
-                        style={{ width: "120px", height: "120px", borderRadius: "50%" }}
+                        style={{ width: "150px", height: "150px", borderRadius: "50%" }}
                       />
-                    </Box>
-                    <Box className="flip-card-back">
-                      {flippedIndex === index && (
-                        <iframe
-                          width="100%"
-                          height="100%"
-                          src={`${item.videoUrl}?autoplay=1`}
-                          frameBorder="0"
-                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                          allowFullScreen
-                          style={{ borderRadius: "12px" }} // Add some styling if needed
-                        ></iframe>
-                      )}
-                    </Box>
-                  </Box>
+                      <PlayArrowIcon
+                        sx={{
+                          position: "absolute",
+                          top: "50%",
+                          left: "50%",
+                          transform: "translate(-50%, -50%)",
+                          color: "white",
+                          fontSize: "3rem",
+                        }}
+                      />
+                    </>
+                  )}
                 </Box>
                 <div>
-                  <Typography fontWeight="larger" gutterBottom>
+                  <Typography
+                    fontWeight="bold"
+                    gutterBottom
+                    align="center"
+                    sx={{ fontSize: "1.5rem", color: "#5392C7" }}
+                  >
                     {item.title}
                   </Typography>
-                  <Typography variant="body2" sx={{ color: "#82BD3E" }}>
+                  <Typography variant="body2" sx={{ color: "#82BD3E", fontSize: "1rem" }}>
                     {item.description}
                   </Typography>
                 </div>
