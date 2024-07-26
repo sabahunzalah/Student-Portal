57eec296f021a2aadc7fecfefb61af9fff2fbcaa
Musarrat-Jahan <mussrratkalam@gmail.com>
2024-07-26 18:00:50 +0500

    card appears with carousel

--- src/LandingPage/components/Pricing.js
+++ src/LandingPage/components/Pricing.js
@@ -1,193 +1,160 @@
 import * as React from "react";
+import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import Chip from "@mui/material/Chip";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Container from "@mui/material/Container";
-import Divider from "@mui/material/Divider";
-import Grid from "@mui/material/Grid";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
-import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 
-const tiers = [
-  {
-    title: "Free",
-    price: "0",
-    description: ["10 users included", "2 GB of storage", "Help center access", "Email support"],
-    buttonText: "Sign up for free",
-    buttonVariant: "outlined",
-  },
-  {
-    title: "Professional",
-    subheader: "Recommended",
-    price: "15",
-    description: [
-      "20 users included",
-      "10 GB of storage",
-      "Help center access",
-      "Priority email support",
-      "Dedicated team",
-      "Best deals",
-    ],
-    buttonText: "Start now",
-    buttonVariant: "contained",
-  },
-  {
-    title: "Enterprise",
-    price: "30",
-    description: [
-      "50 users included",
-      "30 GB of storage",
-      "Help center access",
-      "Phone & email support",
-    ],
-    buttonText: "Contact us",
-    buttonVariant: "outlined",
-  },
+const imageSets = [
+  [
+    {
+      label: "one",
+      imgPath:
+        "https://scontent.fkhi16-2.fna.fbcdn.net/v/t39.30808-6/452257929_122166448466153270_8768805542982941850_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=a06bvGqJ-UwQ7kNvgF8tXPx&_nc_ht=scontent.fkhi16-2.fna&oh=00_AYDUU-gIU5fF4ESPfdoDwkEcTN62Q5zl2ZGPtoJ8Dg3qdw&oe=66A95D17",
+    },
+    {
+      label: "Bird",
+      imgPath:
+        "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+    },
+    {
+      label: "Bali, Indonesia",
+      imgPath:
+        "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+    },
+    {
+      label: "Goč, Serbia",
+      imgPath:
+        "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
+    },
+  ],
+  [
+    {
+      label: "Sunset",
+      imgPath:
+        "https://images.unsplash.com/photo-1541447271453-1b5d4e868a5d?auto=format&fit=crop&w=400&h=250&q=60",
+    },
+    {
+      // label: "Mountain",
+      imgPath:
+        "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?auto=format&fit=crop&w=400&h=250&q=60",
+    },
+    {
+      label: "Forest",
+      imgPath:
+        "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?auto=format&fit=crop&w=400&h=250&q=60",
+    },
+    {
+      label: "Beach",
+      imgPath:
+        "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=400&h=250&q=60",
+    },
+  ],
 ];
 
-export default function Pricing() {
+function SwipeableTextMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = imageSets.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
   return (
-    <Container
-      id="pricing"
-      sx={{
-        pt: { xs: 4, sm: 12 },
-        pb: { xs: 8, sm: 16 },
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: { xs: 3, sm: 6 },
-      }}
-    >
+    // <Box sx={{ maxWidth: 1200, flexGrow: 1 }}>
+    //   <Paper
+    //     square
+    //     elevation={0}
+    //     sx={{
+    //       display: "flex",
+    //       alignItems: "center",
+    //       height: 50,
+    //       pl: 2,
+    //       bgcolor: "background.default",
+    //     }}
+    //   >
+    <Box sx={{ maxWidth: 1200, flexGrow: 1, margin: "0 auto" }}>
       <Box
         sx={{
-          width: { sm: "100%", md: "60%" },
-          textAlign: { sm: "left", md: "center" },
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 4,
         }}
       >
         <Typography component="h2" variant="h4" color="text.primary">
-          Courses
-        </Typography>
-        <Typography variant="body1" color="text.secondary">
-          Quickly build an effective pricing table for your potential customers with this layout.{" "}
-          <br />
-          It&apos;s built with default Material UI components with little customization.
+          New Courses
         </Typography>
       </Box>
-      <Grid container spacing={3} alignItems="center" justifyContent="center">
-        {tiers.map((tier) => (
-          <Grid item key={tier.title} xs={12} sm={tier.title === "Enterprise" ? 12 : 6} md={4}>
-            <Card
-              sx={{
-                p: 2,
-                display: "flex",
-                flexDirection: "column",
-                gap: 4,
-                border: tier.title === "Professional" ? "1px solid" : undefined,
-                borderColor: tier.title === "Professional" ? "primary.main" : undefined,
-                background:
-                  tier.title === "Professional" ? "linear-gradient(#033363, #021F3B)" : undefined,
-              }}
-            >
-              <CardContent>
-                <Box
-                  sx={{
-                    mb: 1,
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    color: tier.title === "Professional" ? "grey.100" : "",
-                  }}
-                >
-                  <Typography component="h3" variant="h6">
-                    {tier.title}
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          height: 50,
+          pl: 2,
+          bgcolor: "background.default",
+        }}
+      >
+        <Typography>{imageSets[activeStep][0].label}</Typography>
+      </Paper>
+      <SwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {imageSets.map((set, index) => (
+          <Box key={index} sx={{ display: "flex", justifyContent: "space-around" }}>
+            {set.map((image) => (
+              <Card key={image.label} sx={{ maxWidth: 300, margin: 1 }}>
+                <CardMedia component="img" height="240" image={image.imgPath} />
+                {/* <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                    {image.label}
                   </Typography>
-                  {tier.title === "Professional" && (
-                    <Chip
-                      icon={<AutoAwesomeIcon />}
-                      label={tier.subheader}
-                      size="small"
-                      sx={{
-                        background: (theme) => (theme.palette.mode === "light" ? "" : "none"),
-                        backgroundColor: "primary.contrastText",
-                        "& .MuiChip-label": {
-                          color: "primary.dark",
-                        },
-                        "& .MuiChip-icon": {
-                          color: "primary.dark",
-                        },
-                      }}
-                    />
-                  )}
-                </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "baseline",
-                    color: tier.title === "Professional" ? "grey.50" : undefined,
-                  }}
-                >
-                  <Typography component="h3" variant="h2">
-                    ${tier.price}
-                  </Typography>
-                  <Typography component="h3" variant="h6">
-                    &nbsp; per month
-                  </Typography>
-                </Box>
-                <Divider
-                  sx={{
-                    my: 2,
-                    opacity: 0.2,
-                    borderColor: "grey.500",
-                  }}
-                />
-                {tier.description.map((line) => (
-                  <Box
-                    key={line}
-                    sx={{
-                      py: 1,
-                      display: "flex",
-                      gap: 1.5,
-                      alignItems: "center",
-                    }}
-                  >
-                    <CheckCircleRoundedIcon
-                      sx={{
-                        width: 20,
-                        color: tier.title === "Professional" ? "primary.light" : "primary.main",
-                      }}
-                    />
-                    <Typography
-                      component="text"
-                      variant="subtitle2"
-                      sx={{
-                        color: tier.title === "Professional" ? "grey.200" : undefined,
-                      }}
-                    >
-                      {line}
-                    </Typography>
-                  </Box>
-                ))}
-              </CardContent>
-              <CardActions>
-                <Button
-                  fullWidth
-                  variant={tier.buttonVariant}
-                  component="a"
-                  href="/material-ui/getting-started/templates/checkout/"
-                  target="_blank"
-                >
-                  {tier.buttonText}
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
+                </CardContent> */}
+              </Card>
+            ))}
+          </Box>
         ))}
-      </Grid>
-    </Container>
+      </SwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+            Next
+            {theme.direction === "rtl" ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === "rtl" ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Back
+          </Button>
+        }
+      />
+    </Box>
   );
 }
+
+export default SwipeableTextMobileStepper;
